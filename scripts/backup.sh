6305694bbcc69604eccec2b62b99e4a50b1cbd75
Tamba Lamin <tamba@tambalamin.me>
2018-04-19 08:06:01 +0000

    date --rfc-3339=seconds: captured latest configuration and backup database using script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,7 +2,7 @@
 backuplocation=/usr/share/nginx/backups/prod.electiondata.io/db
 dev=/usr/share/nginx/dev.electiondata.io/docroot/sites/default
 prod=/usr/share/nginx/prod.electiondata.io/docroot/sites/default
-timestamp=$(date +%s)
+timestamp="date --rfc-3339=seconds"
 vhost=/usr/share/nginx
 cd $prod
 drush status
@@ -15,12 +15,14 @@ git add --all
 git commit -am"$timestamp: captured latest configuration and backup database using script"
 
 cd $vhost
-mv dev.electiondata.io dev.electiondata.io.original
+rm -rf dev.electiondata.io.*
+mv dev.electiondata.io dev.electiondata.io.$timestamp
 cp -R prod.electiondata.io/ dev.electiondata.io/
 mv $dev/settings.php $dev/settings.php.prod
 #mv $dev/settings.php $dev/settings.php.prod
 cp dev.electiondata.io.original/docroot/sites/default/settings.php $dev/settings.php
-
+chown -R tlamin:www-data dev.electiondata.io/
+chown -R www-data:www-data dev.electiondata.io/docroot/sites/default/files/
 cd $dev
 drush status
 drush sql-drop -y
